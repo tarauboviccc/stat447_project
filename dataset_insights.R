c52e0b2303bb2501cb2da64a6ad5d0a4366c8724
vanshikashahh <vanshikashah09@gmail.com>
2025-03-18 11:18:15 -0700

    Added initial data exploration

---
+++ dataset_insights.R
@@ -0,0 +1,8 @@
+online_retail <- read.csv("online_retail.csv")
+online_sales_dataset <- read.csv("online_sales_dataset.csv")
+
+head(online_retail)
+head(online_sales_dataset)
+
+summary(online_retail)
+summary(online_sales_dataset)
